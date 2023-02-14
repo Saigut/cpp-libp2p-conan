6549720aafbe18fa418de7a65c6ed10f68b0d9a0
Saigut <gosaigut@gmail.com>
2023-02-14 18:25:06 +0800

    update cmake files

--- z_other/cmake/helper.cmake
+++ z_other/cmake/helper.cmake
@@ -13,3 +13,13 @@ function(target_link_boost_libraries TargetName)
         target_link_libraries(${TargetName} ${LibPrefix}${arg})
     endforeach()
 endfunction()
+
+function(libp2p_install targets)
+endfunction()
+
+function(libp2p_add_library target)
+    add_library(${target} ${ARGN})
+endfunction()
+
+function(add_proto_library NAME)
+endfunction()
